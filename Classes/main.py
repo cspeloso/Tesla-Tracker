ad72c53781e8454a345fea8772c4ad892a8c5ff0
cspeloso <chrispeloso2@gmail.com>
2024-06-21 02:34:40 -0400

    Fixing config.ini file path issue.

--- Classes/main.py
+++ Classes/main.py
@@ -1,16 +1,23 @@
+import os
 from config import load_config
 from logger import setup_logging
 from scraper import watch_tesla
 
 def main():
-    config = load_config('config.ini')
+
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))    
+    config_path = os.path.join(base_dir, 'config.ini')
+
+    config = load_config(config_path)
+    
     setup_logging(config['files']['log'], config['settings']['debug'])
 
-    run_success = watch_tesla(config, 'y')
-    if run_success:
-        print("Ran successfully for Model Y.\n\n")
-    else:
-        print("Something went wrong.\n\n")
+    #   Disabling Model Y notifications for the time being
+    # run_success = watch_tesla(config, 'y')
+    # if run_success:
+    #     print("Ran successfully for Model Y.\n\n")
+    # else:
+    #     print("Something went wrong.\n\n")
 
     run_success = watch_tesla(config, '3')
     if run_success:
