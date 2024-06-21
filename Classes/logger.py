9575cc539eb89f79c527e7fdec7706431e6f7660
cspeloso <chrispeloso2@gmail.com>
2024-06-21 02:49:20 -0400

    fixing debug config.ini logging issue

--- Classes/logger.py
+++ Classes/logger.py
@@ -2,5 +2,10 @@ import logging
 
 def setup_logging(log_file, debug):
     logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+    
+    if isinstance(debug,str):
+        debug = debug.lower() in ['true','1','yes']
+
     if debug:
+        print('THIS WAS HIT')
         logging.getLogger().setLevel(logging.DEBUG)
